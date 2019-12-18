f3979cc6996caf34ddbcbcac8da424c0b8f8f772
Ket93 <38818395+Ket93@users.noreply.github.com>
2019-12-18 14:13:44 -0500

    Update Space Invaders.java

--- Space Invaders.java
+++ Space Invaders.java
@@ -1,32 +1,81 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 import java.util.*;
 
-public class SpaceInvaders {
+public class SpaceInvaders extends JFrame {
 
+    GamePanel game = new GamePanel();
+
+    public SpaceInvaders (){
+        super ("Space Invaders");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1100,700);
+        setLayout(new BorderLayout());
+        setVisible (true);
+
+    }
+
+    public static void main (String [] args){
+        SpaceInvaders frame = new SpaceInvaders();
+    }
 
 }
 
+class GamePanel extends JPanel{
+    public void background (Graphics pic){
+    pic.setColor(new Color (0,0,0) );
+    pic.fillRect (0,0,getWidth(),getHeight());
+    }
+        }
+
 class Ship{
-    int lives;
-    int score;
-    int position;
+    private int lives;
+    private int score;
+    private int position;
+
+    public Ship(){
+        lives = 3;
+        score  = 0;
+        position = 0;
+    }
+
+    public void move (){
+
+    }
+
+    public void load (){
+
+    }
+
+    public void shoot (){
+
+    }
+
+    public void shield (){
+
+    }
+
+
 
 
 
 }
 
 class Enemy{
-    String enemyType;
-    String bulletType;
-    int bulletSpeed;
-    int enemiesLeft;
-    int positionX;
-    int positionY;
+    private String enemyType;
+    private String bulletType;
+    private int bulletSpeed;
+    private int enemiesLeft;
+    private int positionX;
+    private int positionY;
 
 }
 
 class Bullets{
 
-    
+
 }
+
 
 
