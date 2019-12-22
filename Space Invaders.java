cbe37ad8398f6aa36e55893bebc69fca81c123f5
AdamGaisinsky <59004819+AdamGaisinsky@users.noreply.github.com>
2019-12-22 16:52:16 -0500

    Update Space Invaders.java

--- Space Invaders.java
+++ Space Invaders.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.MouseInfo;
 
 public class SpaceInvaders extends JFrame implements ActionListener {
     Timer myTimer;
@@ -9,7 +10,7 @@ public class SpaceInvaders extends JFrame implements ActionListener {
     public SpaceInvaders (){
       super ("Space Invaders");
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setSize(1100,700);
+      setSize(1000,850);
       
       myTimer = new Timer(10, this);
       myTimer.start();
@@ -17,7 +18,7 @@ public class SpaceInvaders extends JFrame implements ActionListener {
       setVisible (true);
       setResizable (true);
         
-       add(game);
+      add(game);
       
     }
     
@@ -34,81 +35,81 @@ public class SpaceInvaders extends JFrame implements ActionListener {
 }
 
 class GamePanel extends JPanel implements KeyListener{
-  private int x;
+  private Ship ship=new Ship();
+  private Bullet shipBullet=new Bullet("shipBullet");
   private int position;
   private boolean [] keys;
     
   public GamePanel(){
-    x=100;
-    setSize(1100,700);
+    setSize(1000,850);
+    
     keys = new boolean[KeyEvent.KEY_LAST+1];
     addKeyListener(this);
+  }
+  public void addNotify(){
+    super.addNotify();
+    requestFocus();
   }
   
   public void paintComponent(Graphics g){
     g.setColor(new Color(0,0,0));  
-    g.fillRect(0,0,1100,700);  
-    g.setColor(new Color(255,0,0));  
-    g.fillRect(x,600,50,50);
+    g.fillRect(0,0,1000,850);  
+    ship.draw(g);
+    shipBullet.shipBulletDraw(g);
   }
   
   public void move(){
-    x+=1;
     if (keys[KeyEvent.VK_RIGHT]) {
-        position += 5;
-        }
+      ship.move(5);
+    }
     if (keys[KeyEvent.VK_LEFT]) {
-        position -= 5;
-        }
+      ship.move(-5);
     }
-
-    public void keyTyped(KeyEvent keyEvent) {
-
-    }
-
-    public void keyPressed(KeyEvent keyEvent) {
-        keys[keyEvent.getKeyCode()] = true;
-    }
-
-    public void keyReleased(KeyEvent keyEvent) {
-        keys[keyEvent.getKeyCode()] = false;
+    if (keys[KeyEvent.VK_UP]) {
+      shipBullet.shoot(ship.position());
     }
   }
-      
+  
+  public void keyTyped(KeyEvent keyEvent) {
+    
+  }
+  
+  public void keyPressed(KeyEvent keyEvent) {
+    keys[keyEvent.getKeyCode()] = true;
+  }
+  
+  public void keyReleased(KeyEvent keyEvent) {
+    keys[keyEvent.getKeyCode()] = false;
+  }
 }
 
-/*/
 class Ship{
     private int lives;
     private int score;
     private int position;
-
+    private Image shipPic;
+    
     public Ship(){
-        lives = 3;
-        score  = 0;
-        position = 0;
+      lives = 3;
+      score  = 0;
+      position = 100;
+      shipPic= new ImageIcon("SpaceInvadersIMGS/spaceship.png").getImage();
     }
-
-    public void move (){
-
+    public void move (int x){
+      position+=x;
+      if(position<0){
+        position=0;
+      }
+      else if(position>920){
+        position=920;
+      }
     }
-
-    public void load (){
-
+    public void draw(Graphics g){
+      g.drawImage(shipPic,position,650,null);
     }
-
-    public void shoot (){
-
+    public int position(){
+      return position;
     }
-
-    public void shield (){
-
-    }
-
-
-
-
-
 }
 
 class Enemy{
@@ -118,11 +119,29 @@ class Enemy{
     private int enemiesLeft;
     private int positionX;
     private int positionY;
-
 }
 
-class Bullets{
-
-
+class Bullet{
+  private int bx;
+  private int by=650;
+  private String type;
+  private Image bulletPic;
+  
+  public Bullet(String type){
+    bulletPic=new ImageIcon("SpaceInvadersIMGS/Bullets/"+type+".png").getImage();
+  }
+  public void shoot(int x){
+    bx=x+20;
+    by-=10;
+  }
+  public void shipBulletDraw(Graphics g){
+    if(by<650){
+      by-=10;
+      g.drawImage(bulletPic,bx,by,null);
+    }
+    if(by==0){
+      by=650;
+    }
+  }   
 }
-/*/
+
